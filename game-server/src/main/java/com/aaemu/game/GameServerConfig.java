b277ffa202204485a18a955e6e4f563572c0cf1b
Shannon Max <jeyms_bont@mail.ru>
2023-12-09 21:02:13 +0300

    v0.0.2 Game server - Remove Spring WebFlux

--- game-server/src/main/java/com/aaemu/game/GameServerConfig.java
+++ game-server/src/main/java/com/aaemu/game/GameServerConfig.java
@@ -5,9 +5,6 @@ import com.aaemu.game.util.ByteBufUtil;
 import io.netty.channel.Channel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -29,12 +26,5 @@ public class GameServerConfig {
     @Bean
     public Map<Channel, Integer> cookieMap() {
         return new HashMap<>();
-    }
-
-    @Bean
-    public WebClient jsonWebClient() {
-        return WebClient.builder()
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
     }
 }
