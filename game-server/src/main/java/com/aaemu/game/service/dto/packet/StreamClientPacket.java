b9b1c96615a9c56d70543783e910729cb7a896b4
Shannon Max <jeyms_bont@mail.ru>
2023-12-12 21:26:49 +0300

    v0.0.3 Game server - Add log handler - Add support auth packets - Add support proxy packets - Update packet codec - Update exception handler

--- game-server/src/main/java/com/aaemu/game/service/dto/packet/StreamClientPacket.java
+++ game-server/src/main/java/com/aaemu/game/service/dto/packet/StreamClientPacket.java
@@ -9,13 +9,4 @@ public enum StreamClientPacket {
     TC_JOIN_RESPONSE("1");
 
     private final String opcode;
-
-    public static StreamClientPacket getByOpcode(String opcode) {
-        for (StreamClientPacket packet : values()) {
-            if (packet.getOpcode().equalsIgnoreCase(opcode)) {
-                return packet;
-            }
-        }
-        throw new RuntimeException(String.format("Unknown stream server packet opcode: %s", opcode));
-    }
 }
